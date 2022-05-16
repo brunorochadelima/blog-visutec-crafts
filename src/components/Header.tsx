97449179d99417739897e197b51ac8581522a5ca
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-16 14:13:09 -0300

    Ajustando ícones header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/Tema.scss';
 import { ReactComponent as Logo } from 'assets/logo-visutec-crafts.svg';
 import instagram from 'assets/icon-instagram.svg';
+import facebook from 'assets/icon-facebook.svg';
 import InputSearch from './InputSearch';
 import styles from 'components/Header.module.scss';
 
@@ -9,13 +10,11 @@ export default function Header() {
   return (
     <div className="container">
       <div className={styles.flex}>
-        <div>
-          <Logo />
-        </div>
-        <div>
+        <Logo className={styles.logo} />
+        <div className="pb-4">
           <InputSearch />
         </div>
-        <div>
+        <div className={styles.social_icons}>
           <a
             className="mr-3"
             href="https://www.instagram.com/visuteccrafts/"
@@ -25,11 +24,11 @@ export default function Header() {
             <img src={instagram} alt="instagram" />
           </a>
           <a
-            href="https://www.instagram.com/visuteccrafts/"
+            href="https://www.facebook.com/visuteccrafts/?ref=py_c/"
             target="_blank"
             rel="noreferrer"
           >
-            <img src={instagram} alt="instagram" />
+            <img src={facebook} alt="instagram" />
           </a>
         </div>
       </div>

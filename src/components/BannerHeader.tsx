d9cb45cdcdc819e6f0b6a1afb433036ef8b30407
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-16 14:23:21 -0300

    Banner header

--- src/components/BannerHeader.tsx
+++ src/components/BannerHeader.tsx
@@ -10,28 +10,30 @@ export default function BannerHeader() {
   }
   return (
     <div className="container mt-3">
-      <div
-        style={{ borderRadius: '10px' }}
-        className="columns is-vcentered has-text-centered-mobile has-background-link-light p-2"
-      >
-        <div className="column has-text-centered">
-          <img src={FotoCorteLaser} alt="" />
-        </div>
-        <div className="column">
-          <h1 className="title is-1 is-size-3-mobile">
-            Descubra um universo de{' '}
-            <span className="has-text-primary">possibilidades</span>{' '}
-          </h1>
-          <p className="subtitle">
-            Transforme o seu ateliê em um grande negócio
-          </p>
-          <button
-            className="button is-rounded is-primary is-outlined"
-            onClick={lojaVisutecCrafts}
-          >
-            <div className={styles.particle}></div>
-            Loja Visutec Crafts
-          </button>
+      <div className={styles.container}>
+        <div
+          style={{ borderRadius: '10px' }}
+          className="columns is-vcentered has-text-centered-mobile has-background-link-light p-2"
+        >
+          <div className="column has-text-centered">
+            <img src={FotoCorteLaser} alt="" />
+          </div>
+          <div className="column">
+            <h1 className="title is-1 is-size-3-mobile">
+              Descubra um universo de{' '}
+              <span className="has-text-primary">possibilidades</span>{' '}
+            </h1>
+            <p className="subtitle">
+              Transforme o seu ateliê em um grande negócio
+            </p>
+            <button
+              className="button is-rounded is-primary is-outlined"
+              onClick={lojaVisutecCrafts}
+            >
+              <div className={styles.particle}></div>
+              Loja Visutec Crafts
+            </button>
+          </div>
         </div>
       </div>
     </div>

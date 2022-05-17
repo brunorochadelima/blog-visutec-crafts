ede7f924a1ee4c97cdef9599ce25910a48830f42
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-17 15:17:28 -0300

    Efeito hover nos cards

--- src/components/cards/Card.tsx
+++ src/components/cards/Card.tsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import 'styles/Tema.scss';
-import {CardPost} from 'types/CardPost';
+import { CardPost } from 'types/CardPost';
+import styles from './Card.module.scss';
 
 export default function Card(props: CardPost) {
   const { photo, date, title, resume } = props;
 
   return (
-    <div className="card">
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img src={photo} alt="Placeholder image" />
-        </figure>
-      </div>
-      <div className="card-content ">
-        <div className="media">
-          <div className="media-content">
-            <p className="title is-5">{title}</p>
-          </div>
+    <div className={styles.card}>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img src={photo} alt="Placeholder image" />
+          </figure>
         </div>
-        <div className="content has-text-grey-dark">
-          {resume}
-          <p className="pb-2" />
-          <div className="is-flex is-justify-content-space-between">
-            <time>{date}</time>
-            <p className="has-text-primary ">Continue Lendo ❯</p>
+        <div className="card-content ">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-5">{title}</p>
+            </div>
+          </div>
+          <div className="content has-text-grey-dark">
+            {resume}
+            <p className="pb-2" />
+            <div className="is-flex is-justify-content-space-between">
+              <time>{date}</time>
+              <p className="has-text-primary ">Continue Lendo ❯</p>
+            </div>
           </div>
         </div>
       </div>

5fb0f519f5a40076f61c777a62f93f06c38d9dcb
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-18 10:02:09 -0300

    Ajustando rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import Footer from 'components/Footer';
+import Header from 'components/Header';
 import Home from 'pages/Home';
 import Pagina404 from 'pages/Pagina404';
 import Post from 'pages/Post';
@@ -6,15 +7,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 export default function AppRoutes() {
+  const [busca, setBusca] = React.useState('');
   return (
     <main>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<Pagina404 />} />
-          <Route path="post/:id" element={<Post/>}/>
+          <Route path="post/:id" element={<Post />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </main>
   );

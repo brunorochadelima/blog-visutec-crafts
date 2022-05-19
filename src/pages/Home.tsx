c99142c9a87c25cbd0e0cf69d3e7e1616ef62f33
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-19 11:37:10 -0300

    Inserindo banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import BannerHeader from 'components/BannerHeader';
 import GridCards from 'components/cards/GridCards';
 import Header from 'components/Header';
 import React from 'react';
+import styles from 'pages/Home.module.scss';
+import 'styles/Tema.scss';
 
 export default function Home() {
   const [busca, setBusca] = React.useState('');
@@ -17,6 +19,9 @@ export default function Home() {
       <Header busca={busca} setBusca={setBusca} />
       {exibirBanner()}
       <GridCards busca={busca} />
+      <div className="block">
+        <div className={styles.imagemFooter}></div>
+      </div>
     </>
   );
 }

1efe54b1816fb7aa15c275973eecbdfc046f2094
Bruno Rocha de Lima <brunorocha33@live.com>
2022-05-25 17:11:54 -0300

    Removendo props não usadas

--- src/pages/Pagina404.tsx
+++ src/pages/Pagina404.tsx
@@ -8,7 +8,6 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Pagina404() {
   const navigate = useNavigate();
-  const [busca, setBusca] = React.useState('');
   return (
     <div className="container has-text-centered">
       <Header />
